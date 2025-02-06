a22a17552511b702141ca6494ab5fd7daea77ae9
TnAdan-Dev <ademirandrade2129@hotmail.com>
2025-02-06 20:36:41 -0300

    utilizando useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import IconComponent from "./components/IconComponent";
-import ProfileCard from "./components/ProfileCard";
-import StyleCard from "./components/StyleCard";
+import { useState } from "react";
+
+
 
 const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
   return (
     <section>
-      <StyleCard />
-      <ProfileCard />
-      <IconComponent />
+      <h1>{count}</h1>
+      <button onClick={increment} >+</button>
+      <button onClick={decrement} >-</button>
     </section>
   );
 };
